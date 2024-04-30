33637fa3c7ca37c6ee4c06a59002ebb872230f2b
CarlosDevSys <cdealmeidavieira@gmail.com>
2024-04-30 12:13:38 -0300

    Criado projeto ContaBanco

---
+++ ContaBanco/src/com/carlosdevsys/desafio/ContaTerminal.java
@@ -0,0 +1,14 @@
+package com.carlosdevsys.desafio;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+
+		int numeroConta;
+		String agencia;
+		String nomeCliente;
+		double saldo;
+		
+	}
+
+}
