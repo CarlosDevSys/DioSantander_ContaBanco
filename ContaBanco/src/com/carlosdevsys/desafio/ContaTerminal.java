1c382c8fce37f7ac6ba265d559d617a34bfa797e
CarlosDevSys <cdealmeidavieira@gmail.com>
2024-04-30 13:43:28 -0300

    Realizada correções e comentários

--- ContaBanco/src/com/carlosdevsys/desafio/ContaTerminal.java
+++ ContaBanco/src/com/carlosdevsys/desafio/ContaTerminal.java
@@ -1,9 +1,19 @@
+
 package com.carlosdevsys.desafio;
 
 import java.util.Scanner;
 
+/**
+ * @author carlos
+ * @version 1.0.0
+ * @since Release 01
+ * Projeto de aplicação de cadastro de conta bancaria via terminal com objetivo 
+ * de receber os dados via terminal pelo usuario e imprimir em tela as informações formatadas conforme 
+ *  requisito do projeto 
+ *
+ */
 public class ContaTerminal {
-
+  
 	public static void main(String[] args) {
 
 		int numeroConta;
@@ -14,17 +24,17 @@ public class ContaTerminal {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Cadstrtando novo cliente.");
-		System.out.println("Por favor, digite o numero da agencia: ");
+		System.out.print("Por favor, digite o numero da agencia: ");
 		agencia = sc.next();
-		System.out.println("Por favor, digite o numero da conta: ");
+		System.out.print("Por favor, digite o numero da conta: ");
 		numeroConta = sc.nextInt();
 		sc.nextLine();
-		System.out.println("Por favor, digite nome do cliente: ");
+		System.out.print("Por favor, digite nome do cliente: ");
 		nomeCliente = sc.nextLine();
-		System.out.println("Por favor, digiite o saldo inicial da conta: ");
+		System.out.print("Por favor, digiite o saldo inicial da conta: ");
 		saldo = sc.nextDouble();
-		System.out.printf("\"Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque\"", nomeCliente, agencia, numeroConta, saldo );
-		
+		System.out.printf("\"Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo de R$%.2f já está disponível para saque\"", nomeCliente, agencia, numeroConta, saldo );
+		sc.close();
 	}
 
 }
