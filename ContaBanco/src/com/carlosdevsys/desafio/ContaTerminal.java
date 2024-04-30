bf011b8f9ee1949320db93be255163ac4817c196
CarlosDevSys <cdealmeidavieira@gmail.com>
2024-04-30 12:54:14 -0300

    Realizada a codificação base do projeto criando a classe ContaTerminal

--- ContaBanco/src/com/carlosdevsys/desafio/ContaTerminal.java
+++ ContaBanco/src/com/carlosdevsys/desafio/ContaTerminal.java
@@ -1,5 +1,7 @@
 package com.carlosdevsys.desafio;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
@@ -8,6 +10,20 @@ public class ContaTerminal {
 		String agencia;
 		String nomeCliente;
 		double saldo;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Cadstrtando novo cliente.");
+		System.out.println("Por favor, digite o numero da agencia: ");
+		agencia = sc.next();
+		System.out.println("Por favor, digite o numero da conta: ");
+		numeroConta = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Por favor, digite nome do cliente: ");
+		nomeCliente = sc.nextLine();
+		System.out.println("Por favor, digiite o saldo inicial da conta: ");
+		saldo = sc.nextDouble();
+		System.out.printf("\"Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque\"", nomeCliente, agencia, numeroConta, saldo );
 		
 	}
 
